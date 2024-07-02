d9dd59263cf0787996f5daff11208b04f9b3ae88
Ana Laura Possan <102638543+nalaurapozz@users.noreply.github.com>
2024-07-02 11:13:30 -0300

    refactor: add route

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 
+// ROTA
+    // Method HTTP - CRUD (create, read, update, delete)
+        // GET - Pega uma info
+        // POST - cria uma info
+        // PUT - Altera toda a info
+        // PATH - Altera parte da info
+        // DELETE -0 Apaga a info
+
+
+// Name - Um identificador da rota
+// function (Callback) - Responsável por executar algum comando 
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
