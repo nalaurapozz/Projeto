3dcac61e4530515499a120c8d80e7419fd26dcc8
Ana Laura Possan <102638543+nalaurapozz@users.noreply.github.com>
2024-07-04 12:36:18 -0300

    Paginação

--- PROJETO/src/controllers/livros.controller.js
+++ PROJETO/src/controllers/livros.controller.js
@@ -1,14 +1,14 @@
-import { createService, findAllService } from '../services/livros.service.js';
+import { createService, findAllService, countLivros } from '../services/livros.service.js';
 import { ObjectId } from 'mongoose';
 
 const create = async (req, res) => {
     try {
 
         const { name, descricao } = req.body;
-        
+
 
         if (!name || !descricao) {
-            res.status(400).send({ message: "Preencha todos os campos!"});
+            res.status(400).send({ message: "Preencha todos os campos!" });
         }
 
         await createService({
@@ -19,19 +19,63 @@ const create = async (req, res) => {
 
         res.send(201);
     } catch (err) {
-        res.status(500).send({ message: err.message }); 
+        res.status(500).send({ message: err.message });
     }
 
 
-   
+
 };
 
 const findAll = async (req, res) => {
-    const livros = await findAllService();
+    let { limit, offset } = req.query;
+
+    limit = Number(limit);
+    offset = Number(offset);
+
+    if (!limit) {
+        limit = 5;
+    }
+    if (!offset) {
+        offset = 0;
+    }
+
+
+    const livros = await findAllService(offset, limit);
+
+    const total = await countLivros();
+    const currentUrl = req.baseUrl;
+
+    console.log(total);
+
+    const next = offset + limit;
+    const nextUrl = next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null
+
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl = previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null
+
     if (livros.length === 0) {
         return res.status(400).send({ message: "Não há livros cadastrados!" })
     }
-    res.send(livros)
+    res.send({
+        nextUrl,
+        previousUrl,
+        limit,
+        offset,
+        total, 
+
+        results: livros.map((item) => ({
+            id: item._id,
+            name: item.name,
+            descricao: item.descricao,
+            login: item.login,
+            likes: item.likes,
+            comments: item.comments,
+            userName: item.user.nome
+        }))
+
+    });
+
+
 };
 
-export{ create, findAll };
+export { create, findAll };
--- PROJETO/src/services/livros.service.js
+++ PROJETO/src/services/livros.service.js
@@ -2,10 +2,14 @@ import Livros from "../models/Livros.js";
 
 const createService = (body) => Livros.create(body);
 
-const findAllService = () => Livros.find();
+const findAllService = (offset, limit) => Livros.find().sort({ _id: -1 }).skip(offset).limit(limit).populate("user");
+
+const countLivros = () => Livros.countDocuments();
+
 
 
 export {
     createService,
-    findAllService
+    findAllService,
+    countLivros,
 };
